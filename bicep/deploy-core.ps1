12b876aeabd2ee77df5215aa34799397e89c3ec5
Daniel Fang <138417504+qkfang@users.noreply.github.com>
2025-03-01 22:54:32 +0000

    clearn up

--- bicep/deploy-core.ps1
+++ bicep/deploy-core.ps1
@@ -7,14 +7,12 @@ az login
 az account set --subscription {xxx-xxx-xxx-xxx}
 
 # Create a new resource group
-az group create --name {your_rg_name} --location australiaeast
+az group create --name azureaiapp-rg --location australiaeast
 
 #  What-If using the Bicep template and parameters file
-az deployment group create --resource-group {your_rg_name} --template-file ./azuredeploy.bicep --parameters ./azuredeploy.parameters.json --what-if
+az deployment group create --resource-group azureaiapp-rg --template-file ./azuredeploy.bicep --parameters ./azuredeploy.parameters.json --what-if
 
 # Deploy resources using the Bicep template and parameters file
-az deployment group create --resource-group {your_rg_name} --template-file ./azuredeploy.bicep --parameters ./azuredeploy.parameters.json
+az deployment group create --resource-group azureaiapp-rg --template-file ./azuredeploy.bicep --parameters ./azuredeploy.parameters.json
 
-# Deploy GraphRag
-az deployment group create --resource-group {your_rg_name} --template-file ./azuredeploy-graphrag.bicep --parameters ./azuredeploy-graphrag.parameters.json
 
